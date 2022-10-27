4f04b33036291dfc2e584d1cafd40f79b355bcd2
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-10-27 11:45:24 +0100

    Add LGPL-3 license

--- __manifest__.py
+++ __manifest__.py
@@ -2,22 +2,23 @@
 
 
 {
- 'name': 'Stock picking done cancel',
- 'author': 'Soft-integration',
- 'application': False,
- 'installable': True,
- 'auto_install': False,
- 'qweb': [],
- 'description': False,
- 'images': [],
- 'version': '1.0.1.1',
- 'category': 'Stock',
- 'demo': [],
- 'depends': ['stock'],
- 'data': [
-          'security/stock_picking_done_cancel_security.xml',
-          'security/ir.model.access.csv',
-          'views/stock_picking_views.xml',
-          'wizard/stock_picking_force_cancel_confirm_views.xml'
-    ]
- }
+    'name': 'Stock picking done cancel',
+    'author': 'Soft-integration',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1.1',
+    'category': 'Stock',
+    'demo': [],
+    'depends': ['stock'],
+    'data': [
+        'security/stock_picking_done_cancel_security.xml',
+        'security/ir.model.access.csv',
+        'views/stock_picking_views.xml',
+        'wizard/stock_picking_force_cancel_confirm_views.xml'
+    ],
+    'license': 'LGPL-3',
+}
